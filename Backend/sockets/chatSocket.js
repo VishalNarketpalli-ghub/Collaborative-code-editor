d9fdc47562925615522028092ddf0192d1929be0
Vishal Narketpalli <narketpallivishal@gmail.com>
2026-03-17 15:09:36 +0530

    Frontend revision and re formatted backend code corrections in socket

--- Backend/sockets/chatSocket.js
+++ Backend/sockets/chatSocket.js
@@ -1,12 +1,17 @@
-import ChatMessage from "../models/chatMessage.js"
+import ChatMessage from "../models/chatMessage.js";
 
 export default function chatSocket(io, socket) {
-    socket.on("send-message", async ({ roomId, sender, message }) => {
+
+    socket.on("send-message", async ({ roomId, message }) => {
+
+        const userId = socket.user.id;
+
         const chat = await ChatMessage.create({
             room: roomId,
-            sender,
+            sender: userId,
             message
-        })
-        io.to(roomId).emit("receive_message", chat)
-    })
+        });
+
+        io.to(roomId).emit("receive_message", chat);
+    });
 }
