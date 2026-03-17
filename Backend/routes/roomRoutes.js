ed7a74d1bd6e332f540d11e1af4dddeebd3687ad
Pavanika_15 <23eg105v04@anurag.edu.in>
2026-03-17 11:26:28 +0530

    middlewares/routes/jwt

--- Backend/routes/roomRoutes.js
+++ Backend/routes/roomRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import { createRoom, getRoom, joinRoom } from '../controllers/roomController.js'
-
+import {verifyToken} from '../middleware/verifyToken.js'
 const router = express.Router()
 
-router.post("/create", createRoom)
-router.post("/join", joinRoom)
-router.post("/:roomId", getRoom)
+router.post("/create",verifyToken, createRoom)
+router.post("/join",verifyToken, joinRoom)
+router.post("/:roomId",verifyToken, getRoom)
 
 export default router
